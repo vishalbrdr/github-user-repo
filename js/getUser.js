bb19e94107c5c4142a9aea295447cbed5fed895c
vishalbrdr <biradarvishal290@gmail.com>
2024-01-20 17:54:01 +0530

    Commented out authorization headers in getUser.js and getUserRepos.js

--- js/getUser.js
+++ js/getUser.js
@@ -2,10 +2,10 @@ async function getUser(username) {
   try {
     loader.start();
     const res = await fetch(`https://api.github.com/users/${username}`, {
-      headers: {
-        Authorization:
-          "Bearer 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",
-      },
+      // headers: {
+      //   Authorization:
+      //     "Bearer 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",
+      // },
     });
     // if (!res.ok) return { user: null, error: true };
     const user = await res.json();
--- js/getUserRepos.js
+++ js/getUserRepos.js
@@ -3,12 +3,12 @@ async function getUserRepos(username, page = 1, perpage = 10) {
     loader.start();
     const res = await fetch(
       `https://api.github.com/users/${username}/repos?per_page=${perpage}&page=${page}`,
-      {
-        headers: {
-          Authorization:
-            "Bearer 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",
-        },
-      }
+      // {
+      //   headers: {
+      //     Authorization:
+      //       "Bearer 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",
+      //   },
+      // }
     );
     if (!res.ok) return null;
     const repos = await res.json();
